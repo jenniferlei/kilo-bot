c83bb9da1182563775f66cb84a3478da0d746143
Jennifer Lei <jenniferlei1928@gmail.com>
2021-11-23 01:45:24 +0000

    Bot success

--- kilo.py
+++ kilo.py
@@ -1,66 +1,38 @@
 """A Markov chain generator that can tweet random messages."""
 
 import sys
-from random import choice
+import os
+import discord
+import requests
+import json
 
 
-def open_and_read_file(filenames):
-    """Take list of files. Open them, read them, and return one long string."""
+def get_random_stoic_quote():
+    """Takes a random quote and turns it into a string."""
 
-    body = ''
-    for filename in filenames:
-        text_file = open(filename)
-        body = body + text_file.read()
-        text_file.close()
+    source = requests.get("https://stoicquotesapi.com/v1/api/quotes/random")
+    quote = json.loads(source.text)
 
-    return body
+    return f"""{quote['body']}
+- {quote['author']}"""
 
 
-def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains."""
-
-    chains = {}
-
-    words = text_string.split()
-    for i in range(len(words) - 2):
-        key = (words[i], words[i + 1])
-        value = words[i + 2]
-
-        if key not in chains:
-            chains[key] = []
-
-        chains[key].append(value)
-
-    return chains
+# Connect to discord
+client = discord.Client()
 
 
-def make_text(chains):
-    """Take dictionary of Markov chains; return random text."""
-
-    keys = list(chains.keys())
-    key = choice(keys)
-
-    words = [key[0], key[1]]
-    while key in chains:
-        # Keep looping until we have a key that isn't in the chains
-        # (which would mean it was the end of our original text).
-
-        # Note that for long texts (like a full book), this might mean
-        # it would run for a very long time.
-
-        word = choice(chains[key])
-        words.append(word)
-        key = (key[1], word)
-
-    return ' '.join(words)
+@client.event
+async def on_ready():
+    print(f'Successfully connected! Logged in as {client.user}.')
 
 
-# Get the filenames from the user through a command line prompt, ex:
-# python markov.py green-eggs.txt shakespeare.txt
-filenames = sys.argv[1:]
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
 
-# Open the files and turn them into one long string
-text = open_and_read_file(filenames)
+    if "kilo" in message.content or "Kilo" in message.content:
+        await message.channel.send(get_random_stoic_quote())
 
-# Get a Markov chain
-chains = make_chains(text)
+
+client.run(os.environ['DISCORD_TOKEN'])
